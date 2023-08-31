babe5adea6d9b05edb6bbc34b09809031b61d389
Daniel Quansah <114119228+PyDanny21@users.noreply.github.com>
2023-08-30 23:25:37 -0700

    Add files via upload

---
+++ get_weather.py
@@ -0,0 +1,12 @@
+OPENWEATHER_APP_ID = "OPENWEATHER_APP_ID"
+import requests
+
+
+def get_weather_report(city):
+    res = requests.get(
+        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_APP_ID}&units=metric").json()
+    weather = res["weather"][0]["main"]
+    temperature = res["main"]["temp"]
+    feels_like = res["main"]["feels_like"]
+    return weather, f"{temperature}℃", f"{feels_like}℃"
+get_weather_report(Accra)
